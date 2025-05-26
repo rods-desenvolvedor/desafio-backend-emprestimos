983cf28b3da4775825bfd1596acfaec250d6dbfe
rods <rods.desenvolvedor@gmail.com>
2025-05-26 16:55:20 -0400

    feat: adicionado construtor no TipoEmprestimo definindo a taxa

--- src/main/java/com/desafio/emprestimo/entity/enums/TipoEmprestimo.java
+++ src/main/java/com/desafio/emprestimo/entity/enums/TipoEmprestimo.java
@@ -1,7 +1,21 @@
 package com.desafio.emprestimo.entity.enums;
 
+import java.math.BigDecimal;
+
 public enum TipoEmprestimo {
-    PESSOAL,
-    CONSIGNADO,
-    GARANTIA
+    PESSOAL(new BigDecimal("4.0")),
+    CONSIGNADO(new BigDecimal("2.0")),
+    GARANTIA(new BigDecimal("3.0"));
+
+    private final BigDecimal taxa;
+
+    TipoEmprestimo(BigDecimal taxa)
+    {
+        this.taxa = taxa;
+    }
+
+    public BigDecimal getTaxa()
+    {
+        return this.taxa;
+    }
 }
