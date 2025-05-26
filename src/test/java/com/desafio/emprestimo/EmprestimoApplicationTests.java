34b0f6eb05b26229136fdb21c5875cd2b2d957c3
rods <rods.desenvolvedor@gmail.com>
2025-05-26 09:47:18 -0400

    chore: commit inicial

---
+++ src/test/java/com/desafio/emprestimo/EmprestimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.emprestimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
